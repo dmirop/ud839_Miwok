a52a1597e94b4e5c6137e82ca6c7b2c3c6d07913
dmirop <daniel.miro.pettican@gmail.com>
2017-08-02 20:24:12 +0200

    Lesson 2.28

---
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -0,0 +1,40 @@
+package com.example.android.miwok;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by daniel on 2/08/17.
+ */
+
+public class WordAdapter extends ArrayAdapter<Word> {
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View listItemView = convertView;
+        if (listItemView == null){
+            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent,false);
+        }
+
+        Word currentWord = getItem(position);
+        TextView defaultTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
+        defaultTextView.setText(currentWord.getDefaultTranslation());
+
+        TextView miwokTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
+        miwokTextView.setText(currentWord.getMiwokTranslation());
+
+        return listItemView;
+
+    }
+
+    public WordAdapter(Activity context, ArrayList<Word> arrayListWords){
+        super(context,0,arrayListWords);
+
+    }
+}
