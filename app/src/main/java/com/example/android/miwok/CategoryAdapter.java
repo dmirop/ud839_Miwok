4c8e688aef3b90451a7273c5fcd2558c0fffed3d
dmirop <daniel.miro.pettican@gmail.com>
2017-08-05 23:58:35 +0200

    Lesson 6.13

--- app/src/main/java/com/example/android/miwok/CategoryAdapter.java
+++ app/src/main/java/com/example/android/miwok/CategoryAdapter.java
@@ -1,6 +1,7 @@
 package com.example.android.miwok;
 
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -11,8 +12,14 @@ import android.support.v4.app.FragmentPagerAdapter;
 
 public class CategoryAdapter extends FragmentPagerAdapter {
 
-    public CategoryAdapter(FragmentManager fm){
+    private Context mContext;
+
+    final int PAGE_COUNT = 4;
+    private String tabTitles[] = new String[]{"NUMBERS", "FAMILY", "COLORS", "PHRASES"};
+
+    public CategoryAdapter(Context context, FragmentManager fm){
         super(fm);
+        mContext = context;
     }
 
     @Override
@@ -30,6 +37,19 @@ public class CategoryAdapter extends FragmentPagerAdapter {
 
     @Override
     public int getCount() {
-        return 4;
+        return PAGE_COUNT;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if (position == 0){
+            return mContext.getString(R.string.category_numbers);
+        } else if (position == 1){
+            return mContext.getString(R.string.category_family);
+        } else if (position ==2){
+            return mContext.getString(R.string.category_colors);
+        } else {
+            return mContext.getString(R.string.category_phrases);
+        }
     }
 }
